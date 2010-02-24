9a47dddc2f63ae0544a4c66f217b608ae63dbe1e
Peter Measham <pmeasham@gmail.com>
2010-02-24 23:35:34 +0000

    improvements from pylint

--- songbot.py
+++ songbot.py
@@ -20,23 +20,39 @@ from twisted.internet import protocol
 
 import datetime
 import logging
+
 log = logging.getLogger('songbot')
 
 class SongBot(irc.IRCClient):
-    
+    """
+    Client class to handle joining the correct channel
+    """
+   
     @property
     def nickname(self):
+        """
+        Get nick from factory
+        """
         return self.factory.nickname
 
     def signedOn(self):
+        """
+        Join channel when signed on to irc
+        """
         log.info('Signed on')
         self.join(self.factory.channel)
 
     def joined(self, channel):
+        """
+        When in channel update factory class with intance of client
+        """
         log.info('Joined channel: %s' %  channel)
         self.factory.client = self
 
 class SongBotFactory(protocol.ClientFactory):
+    """
+    Handle client and send song infomation
+    """
     protocol = SongBot
     client   = None
 
@@ -45,35 +61,56 @@ class SongBotFactory(protocol.ClientFactory):
         self.nickname = nickname
 
     def clientConnectionLost(self, connector, reason):
+        """
+        Shutdown on connection close
+        """
         shutdown()
 
     def clientConnectionFailed(self, connector, reason):
+        """
+        Display connection error and close
+        """
         log.error('Could not connect: %s' % str(reason))
         shutdown()
 
     def sendSong(self, name):
+        """
+        Push song information to irc client
+        """
         if self.client is not None:
             self.client.me(self.channel, str(name))
 
 class Rhythmbox:
-    lastSent = None
+    """
+    Loads dbus interfaces and signals to irc
+    """
+    sent = None
 
-    def __init__(self, irc):
-        self.irc  = irc
+    def __init__(self, factory):
+        self.irc  = factory
         session   = dbus.SessionBus()
 
-        player = session.get_object('org.gnome.Rhythmbox', '/org/gnome/Rhythmbox/Player')
-        self.rhythmPlayer = dbus.Interface(player, 'org.gnome.Rhythmbox.Player')
+        oplayer = session.get_object('org.gnome.Rhythmbox', \
+            '/org/gnome/Rhythmbox/Player')
+        
+        self.player = dbus.Interface(oplayer, 'org.gnome.Rhythmbox.Player')
 
-        shell = session.get_object('org.gnome.Rhythmbox', '/org/gnome/Rhythmbox/Shell')
-        self.rhythmShell = dbus.Interface(shell, 'org.gnome.Rhythmbox.Shell')
+        oshell = session.get_object('org.gnome.Rhythmbox', \
+            '/org/gnome/Rhythmbox/Shell')
+        
+        self.shell = dbus.Interface(oshell, 'org.gnome.Rhythmbox.Shell')
 
-        session.add_signal_receiver(self.songChange, dbus_interface='org.gnome.Rhythmbox.Player', signal_name='playingUriChanged')
+        session.add_signal_receiver(self.songChange, \
+            dbus_interface='org.gnome.Rhythmbox.Player', \
+            signal_name='playingUriChanged')
 
     def songChange(self, *args, **kwargs):
+        """
+        callback function for file uri updates in rhythmbox
+        """
         try:
-            uri = self.rhythmPlayer.getPlayingUri()
-            details = self.rhythmShell.getSongProperties(uri)
+            uri = self.player.getPlayingUri()
+            details = self.shell.getSongProperties(uri)
            
             if self.rateCheck():
                 song = '%s - %s' % (details['artist'], details['title'])
@@ -81,27 +118,33 @@ class Rhythmbox:
             
                 log.info('/me is now playing: %s' % song)
                 self.irc.sendSong('is now playing: 3%s' % song)
-        except Exception, e:
-            log.error('Could not access Rhythmbox closing')
+        except Exception, exc:
+            log.error('Could not access Rhythmbox closing: %s' % str(exc))
             shutdown()
 
     def rateCheck(self):
+        """
+        Check its been 5 minutes since last irc update
+        """
         check = False
         now = datetime.datetime.today()
-        if self.lastSent is None:
+        if self.sent is None:
             check = True
 
         if not check:
             ago = now - datetime.timedelta(minutes=5)
-            if self.lastSent < ago:
+            if self.sent < ago:
                 check = True
 
         if check is True:
-            self.lastSent = now
+            self.sent = now
 
         return check
 
-def initializeLogging(options):
+def setup_logging(options):
+    """
+    Setup log to output to terminal
+    """
     logger = logging.getLogger('songbot')
     handler = logging.StreamHandler()
 
@@ -117,9 +160,15 @@ def initializeLogging(options):
     return logger
 
 def shutdown():
+    """
+    Add deferred to shutdown the twisted event loop
+    """
     reactor.callLater(1, reactor.stop)	
 
 def main():
+    """
+    Parse options and setup twisted
+    """
     from optparse import OptionParser
 
     usage = 'usage: %prog [options] network port channel nick'
@@ -133,11 +182,11 @@ def main():
     if not args[2].startswith('#'):
         args[2] = '#%s' % args[2]
 
-    initializeLogging(options)
+    setup_logging(options)
 
-    irc = SongBotFactory(args[2], args[3])
-    Rhythmbox(irc)
-    reactor.connectTCP(args[0], int(args[1]), irc)
+    song = SongBotFactory(args[2], args[3])
+    Rhythmbox(song)
+    reactor.connectTCP(args[0], int(args[1]), song)
 
     reactor.run()
 
